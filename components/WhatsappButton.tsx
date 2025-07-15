6bbcf45a57552f2ffef9caa96f99dcf604eb7797
Major41 <kevomajor41@gmail.com>
2025-07-15 12:17:40 +0300

    whatsapp

--- components/WhatsappButton.tsx
+++ components/WhatsappButton.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function WhatsappButton() {
   return (
     <Link
-      href="https://wa.me/2540105205150"
+      href="https://wa.me/16056718890"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-4 rounded-full shadow-lg transition duration-300"
